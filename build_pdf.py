bc7c21c3430ca1112ad5ca76aa37ca6342fb3010
Luke <spambigbox@gmail>
2025-08-18 18:05:40 +0200

    Added yaml translations

--- build_pdf.py
+++ build_pdf.py
@@ -1,10 +1,24 @@
 from weasyprint import HTML, CSS
+from jinja2 import Environment, FileSystemLoader
+import yaml, sys
 
-html = HTML('static/cv.html')
-css = CSS('static/styles/cv.css')
+with open('static/content.yaml', "r", encoding="utf-8") as f:
+    data = yaml.safe_load(f)
 
-html.write_pdf(
-    'Lukasz Wisniewski CV.pdf',
-    stylesheets=[css],
-    optimize_size=()
-)
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('static/cv_template.html')
+
+html_toggle = template.render(content=data)
+open('static/cv_toggle.html', 'w', encoding="utf-8").write(html_toggle)
+
+html_en=template.render(content={"en": data["en"]})
+HTML(string=html_en, base_url=".").write_pdf("CV_en.pdf", stylesheets=[CSS('static/styles/cv.css')])
+
+# html = HTML('static/cv.html')
+# css = CSS('static/styles/cv.css')
+#
+# html.write_pdf(
+#     'Lukasz Wisniewski CV.pdf',
+#     stylesheets=[css],
+#     optimize_size=()
+# )
